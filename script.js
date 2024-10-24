834f6d604cf02b5260fa93854088a9d6c65df0e4
VytasKitm <vytautas.cerniauskas@stud.kitm.lt>
2024-10-24 09:05:20 +0300

    atsiskaitymas

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+let amzius = 37;
+
+amzius > 18 ? console.log("Suauges."):console.log("Vaikas.");
+
+
+
+
+const skaicius = 3;
+const zodis = "tipas";
+
+function tipas(a) {
+if (typeof(a) == "number") {
+    console.log(`Kintamasis ${a} yra skaicius.`);
+}
+else if (typeof(a) == "string") {
+    console.log(`Kintamasis ${a} yra string.`);
+}
+}
+
+tipas(skaicius);
+tipas(zodis);
+
+
+
+
+
+
+
+function max() {
+    const a = prompt("Iveskite pirma skaiciu:");
+    const b = prompt("Iveskite antra skaiciu:");
+    const c = prompt("Iveskite trecia skaiciu:");
+    const max = Math.max(a,b,c);
+    alert(`Didziausias skaicius yra: ${max}`);
+}
+
+max();
